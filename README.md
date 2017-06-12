2f9d1827cbb5a485bf5b26d9ab811b5123570836
Cooky90 <ashley.m.d.cook@gmail.com>
2017-06-12 01:18:18 +0100

    Completed description tasks and completed all tasks using npm test to check if answers correct

--- README.md
+++ README.md
@@ -5,9 +5,13 @@
 1. Feynman Writing Prompts - Write out explanations of the following concepts like you are explaining it to a 12 year old.  Doing this will help you quickly discover any holes in your understanding.  Ask your questions on Slack.
 
 	* Functions
-	* Parameters
-	* Arguments
+-A function is a block of code that is kept together to form a specific function. For example, you could create a function to "add two numbers together", which would require two lines of code, one to add the numbers together and another to output calculation that is returned. This block of code needs to be assigned (=) to a function name so it can be called on again in the program i.e. var addTwoNumbersTogether() = function() {**code**}
+
+	* Parameters & Arguments
+-Parameters can be described as placeholders in functions, in which data can be passed into the function at the point of execution. So, the previous function we described could have have two parameters which would look like the following, addTwoNumbersTogether(x,y). x and y in this case are the placeholders for the two numbers we wish to add together, if addTwoNumbersTogether(5,5) was used then we'd have the number 10 returned from the code executed. These are also known as the function arguments which are required to be passed to the function during the point of execution.
+
 	* `if` statements
+-An if statement is used to determine whether a piece of code should run depending on whether requirements are met. For example, an IF statement could be used to check whether a number passed to a function called addTwoEvenNumbersTogether(); is in fact an even number if not. The number would be run past the if statement first, and if the number passed to the function is not an even number then an error message could be returned to the program.
 
 2. Install Node and NPM.  NPM comes packaged with Node. https://nodejs.org/en/download/
 
--- exercises.js
+++ exercises.js
@@ -1,114 +1,167 @@
 //Do not change any of the function names
+// load math.js (using node.js)
+var math = require('mathjs');
 
 function multiplyByTen(num) {
   //return num after multiplying it by ten
+  return num * 10;
+
 }
 
 function subtractFive(num) {
   //return num after subtracting five
+  return num - 5;
 }
 
 function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
+  if (str1.length === str2.length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
+  if (x === y) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
+  if (num < 90) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function greaterThanFifty(num) {
   //return true if num is greater than fifty
   //otherwise return false
+  if (num > 50) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function add(x, y) {
   //add x and y together and return the value
+  return x + y;
 }
 
 function subtract(x, y) {
   //subtract y from x and return the value
+  return x - y;
 }
 
 function divide(x, y) {
   //divide x by y and return the value
+  return x / y;
 }
 
 function multiply(x, y) {
   //multiply x by y and return the value
+  return x * y;
 }
 
 function getRemainder(x, y) {
   //return the remainder from dividing x by y
+  return x % y;
 }
 
 function isEven(num) {
   //return true if num is even
   //otherwise return false
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isOdd(num) {
   //return true if num is odd
   //otherwise return false
+  if (num % 2 === 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function square(num) {
   //square num and return the new value
+  return num * num;
 }
 
 function cube(num) {
   //cube num and return the new value
+  return num * num * num;
 }
 
 function raiseToPower(num, exponent) {
   //raise num to whatever power is passed in as exponent
+  return math.pow(num, exponent);
 }
 
 function roundNumber(num) {
   //round num and return it
+  return math.round(num);
 }
 
 function roundUp(num) {
   //round num up and return it
+  return math.ceil(num);
 }
 
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
+  return str + '!';
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
+  return firstName + ' ' + lastName;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
+  return 'Hello' + name;
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
 
 function getRectangleArea(length, width) {
   //return the area of the rectangle by using length and width
+  return length * width;
 }
 
 function getTriangleArea(base, height) {
   //return the area of the triangle by using base and height
+  base * height / 2;
 }
 
 function getCircleArea(radius) {
   //return the rounded area of the circle given the radius
+  return radius * radius * math.PI;
 }
 
 function getRectangularPrismVolume(length, width, height) {
   //return the area of the 3D rectangular prism given the length, width, and height
+  return (length * height) * width;
+
 }
 
 //Do not modify code below this line.
